8358eee4b438085e4cfe1dedea9367748019026f
Rodi26 <30529563+Rodi26@users.noreply.github.com>
2019-07-03 15:02:54 +0200

    Update scenario1.tf

--- BlueMix/terraform/hcl/scenario1/scenario1.tf
+++ BlueMix/terraform/hcl/scenario1/scenario1.tf
@@ -1,39 +1,56 @@
-provider "ibm" {}
+#################################################################
+# Terraform template that will deploy an VM with Node.js only
+#
+# Version: 1.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Licensed Materials - Property of IBM
+#
+# ©Copyright IBM Corp. 2017.
+#
+#################################################################
 
+#########################################################
+# Define the ibmcloud provider
+#########################################################
+provider "ibm" {
+}
+
+#########################################################
+# Helper module for tagging
+#########################################################
 module "camtags" {
   source = "../Modules/camtags"
 }
 
-variable "public_ssh_key" {
-  description = "Public SSH key used to connect to the virtual guest"
-}
+#########################################################
+# Define the variables
+#########################################################
 variable "datacenter" {
   description = "Softlayer datacenter where infrastructure resources will be deployed"
 }
 
 variable "hostname" {
-  description = "Hostname of the virtual instance (small flavor) to be deployed"
-  default     = "debian-small"
+  description = "Hostname of the virtual instance to be deployed"
 }
 
-# This will create a new SSH key that will show up under the \
-# Devices>Manage>SSH Keys in the SoftLayer console.
-resource "ibm_compute_ssh_key" "orpheus_public_key" {
-  label      = "Orpheus Public Key"
-  public_key = "${var.public_ssh_key}"
+variable "public_ssh_key" {
+  description = "Public SSH key used to connect to the virtual guest"
 }
 
-variable "domain" {
-  description = "VM domain"
-}
 
 ##############################################################
 # Create public key in Devices>Manage>SSH Keys in SL console
 ##############################################################
-#resource "ibm_compute_ssh_key" "cam_public_key" {
-#  label      = "CAM Public Key"
-#  public_key = "${var.public_ssh_key2}"
-#}
+resource "ibm_compute_ssh_key" "cam_public_key" {
+  label      = "CAM Public Key"
+  public_key = "${var.public_ssh_key}"
+}
 
 ##############################################################
 # Create temp public key for ssh connection
@@ -47,56 +64,68 @@ resource "ibm_compute_ssh_key" "temp_public_key" {
   public_key = "${tls_private_key.ssh.public_key_openssh}"
 }
 
- 
-# Create a new virtual guest using image "Debian"
-resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
+##############################################################
+# Create Virtual Machine and install MongoDB
+##############################################################
+resource "ibm_compute_vm_instance" "softlayer_virtual_guest" {
   hostname                 = "${var.hostname}"
-  os_reference_code        = "DEBIAN_8_64"
-  domain                   = "${var.domain}"
+  os_reference_code        = "CENTOS_7_64"
+  domain                   = "cam.ibm.com"
   datacenter               = "${var.datacenter}"
   network_speed            = 10
   hourly_billing           = true
   private_network_only     = false
   cores                    = 1
   memory                   = 1024
-  disks                    = [25, 10, 20]
-  user_metadata            = "{\"value\":\"newvalue\"}"
+  disks                    = [25]
   dedicated_acct_host_only = false
   local_disk               = false
-  ssh_key_ids              = ["${ibm_compute_ssh_key.orpheus_public_key.id}"]
+  ssh_key_ids              = ["${ibm_compute_ssh_key.cam_public_key.id}", "${ibm_compute_ssh_key.temp_public_key.id}"]
   tags                     = ["${module.camtags.tagslist}"]
-
-
 
   # Specify the ssh connection
   connection {
     user        = "root"
-    #private_key = "${tls_private_key.ssh.private_key_pem}"
     private_key = "${tls_private_key.ssh.private_key_pem}"
-             
+    host        = "${self.ipv4_address}"
+    bastion_host        = "${var.bastion_host}"
+    bastion_user        = "${var.bastion_user}"
+    bastion_private_key = "${ length(var.bastion_private_key) > 0 ? base64decode(var.bastion_private_key) : var.bastion_private_key}"
+    bastion_port        = "${var.bastion_port}"
+    bastion_host_key    = "${var.bastion_host_key}"
+    bastion_password    = "${var.bastion_password}"
   }
-     provisioner "file" {
+
+  # Create the installation script
+  provisioner "file" {
     content = <<EOF
-    #!/bin/bash
-    # update ubuntu
-    sudo apt-get update
-    # install NAGIOS nagios_client
-    sudo apt-get install nagios-nrpe-server nagios-plugins
-    sed -i "s@#server_address=127.0.0.1@server_address=169.62.141.140@g" /etc/nagios/nrpe.cfg
-    service nagios-nrpe-server restart
-    EOF
-    
+    resource "null_resource" "install_client" {
+      provisioner "file" {
+        content = << EOF
+        #!/bin/bash
+        # update ubuntu
+        sudo apt - get update
+        # install NAGIOS nagios_client
+        sudo apt - get install nagios - nrpe - server nagios - plugins
+        sed - i "s@#server_address=127.0.0.1@server_address=169.62.141.140@g" / etc / nagios / nrpe.cfg
+        service nagios - nrpe - server restart
+        EOF
+
+
     destination = "/tmp/installation.sh"
-    }
-  
+  }
+
+  # Execute the script remotely
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/installation.sh; bash /tmp/installation.sh "
+      "chmod +x /tmp/installation.sh; bash /tmp/installation.sh",
     ]
   }
 }
-   
-    
-output "vm_ip" {
-  value = "Public : ${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
+
+#########################################################
+# Output
+#########################################################
+output "The IP address of the VM with NodeJs installed" {
+  value = "${ibm_compute_vm_instance.softlayer_virtual_guest.ipv4_address}"
 }
